4bf01ee42d9242fdb94f78a557fd3a82644aedcd
Gamingninduwa <Induwaraheroku@gmail.com>
2024-10-10 20:14:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "enter mongodb here",
+MONGODB: process.env.MONGODB || "0ylof9nss36v5mgpeopy4mqrxszrzvqb",
 };
